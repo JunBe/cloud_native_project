92a54c7a8c18026f8297fb85c2ad25cfca2a016c
JunBe <wnsqowid0901@gmail.com>
2024-11-04 23:05:37 +0900

    setting: MySQL 설정

--- backend/budgettracker/README.md
+++
@@ -1,2 +0,0 @@
-# cloud_native_project
-클라우드 네이티브 기말 프로젝트입니다.
--- backend/budgettracker/src/main/java/com/myaccountbook/budgettracker/BudgettrackerApplication.java
+++ backend/budgettracker/src/main/java/com/myaccountbook/budgettracker/BudgettrackerApplication.java
@@ -8,6 +8,13 @@ public class BudgettrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BudgettrackerApplication.class, args);
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("MySQL 드라이버 로드 성공"); //성공
+		} catch (ClassNotFoundException e) {
+			System.out.println("MySQL 드라이버 로드 실패");
+			e.printStackTrace();
+		}
 	}
 
 }
