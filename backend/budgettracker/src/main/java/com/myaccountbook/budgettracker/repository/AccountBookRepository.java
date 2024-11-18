8c4b76c2dc83267b99fe2a5f7a099a5d8af27c87
JunBe <wnsqowid0901@gmail.com>
2024-11-18 18:16:06 +0900

    feat: JPA를 사용을 위한 Repository 생성
    JPA를 이용하기 위한 Repository를 생성했고, 사용 할 메서드들을 추가로 정의했다.

--- backend/budgettracker/src/main/java/com/myaccountbook/budgettracker/repository/AccountBookRepository.java
+++ backend/budgettracker/src/main/java/com/myaccountbook/budgettracker/repository/AccountBookRepository.java
@@ -2,7 +2,21 @@ package com.myaccountbook.budgettracker.repository;
 
 import com.myaccountbook.budgettracker.entity.AccountBook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AccountBookRepository extends JpaRepository<AccountBook, Integer> {
 
+    List<AccountBook> findByType(AccountBook.Type type);
+
+    //날짜 기준 오름차순 정렬
+    List<AccountBook> findByTypeOrderByDateAsc(AccountBook.Type type);
+
+    //날짜 기준 내림차순 정렬
+    List<AccountBook> findByTypeOrderByDateDesc(AccountBook.Type type);
+
+    @Query("SELECT a.category, SUM(a.amount) FROM AccountBook a WHERE YEAR(a.date) = :year AND MONTH(a.date) = :month GROUP BY a.category")
+    List<Object[]> findMonthlyStatistics(@Param("year") int year, @Param("month") int month);
 }
