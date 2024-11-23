3183b170904f695bf03cf8f6943b138db502db8d
JunBe <wnsqowid0901@gmail.com>
2024-11-23 21:25:16 +0900

    fix: CORS 오류 해결
    클라이언트에서 요청을 보낼 시 CORS 오류가 발생하여 해당 IP에서 요청을 보냈을 때 CORS 오류가 발생하지 않도록 수정했다.

--- backend/budgettracker/src/main/java/com/myaccountbook/budgettracker/config/WebConfig.java
+++ backend/budgettracker/src/main/java/com/myaccountbook/budgettracker/config/WebConfig.java
@@ -8,9 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // API 경로에 대한 CORS 허용
-                .allowedOrigins("http://localhost:5500") // 프론트엔드 실행 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
+        registry.addMapping("/**") // API 경로에 대한 CORS 허용
+                .allowedOrigins("http://192.168.152.128") // 프론트엔드 실행 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .exposedHeaders("Set-Cookie");
     }
 }
